5547abeb02f991f10806b8929083ce7626460c3a
rakibhasan1868 <rakibhasanr587@gmail.com>
2025-08-29 14:26:19 +0600

    copy number section added

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+function getTextContent(element) {
+    let number = document.getElementById(element).innerText;
+    return number;
+}
+function increaseCount(element, increment) {
+    let count = document.getElementById(element);
+    count.innerText = parseInt(count.innerText) + increment;
+}
+function decreaseCount(element, decrement) {
+    let count = document.getElementById(element);
+    count.innerText = Math.max(0, parseInt(count.innerText) - decrement);
+}
+function copyContent(title, number) {
+    navigator.clipboard.writeText(number).then(function () {
+        let copyCount = getTextContent("copy");
+        copyCount = increaseCount("copy", 1);
+        alert('Copied ' + title + ": " + number);
+    }, function (err) {
+        console.error('Could not copy text: ', err);
+});
+}
+
 function toggleHeart(icon) {
     let heartCount = document.getElementById("heart");
 
